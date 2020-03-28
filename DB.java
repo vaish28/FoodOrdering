d3055c9eb0ee9c3b6642d57c83b60bbbd5a78c3a
anaya1012 <56404064+anaya1012@users.noreply.github.com>
2020-03-28 14:37:29 +0530

    Create DB.java

---
+++ DB.java
@@ -0,0 +1,26 @@
+package com;
+import java.sql.*;
+public class DB {
+
+	public static Connection conc()
+	{
+		Connection con;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String url=("jdbc:mysql://localhost/dabewala");
+
+			 con=DriverManager.getConnection(url,"root","Archanasanjeev@99");
+			
+			 return con;
+		}
+		catch(Exception e)
+		{
+			System.out.println("exception"+e);
+			return null;
+		} 
+		
+	}
+
+}
